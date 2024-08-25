232db4e09858de4b5f554be42cd5aa10c32c77fe
kevin <kevintorregrosa2304@gmail.com>
2024-08-25 00:06:13 -0500

    fixed UTC format and switched to 24 hour clock

--- public/update.js
+++ public/update.js
@@ -5,12 +5,27 @@ function fetchLatestLocation() {
             document.getElementById('latitude').innerText = data.Latitude;
             document.getElementById('longitude').innerText = data.Longitude;
             document.getElementById('timestamp').innerText =
-                `${data.Timestamp.substring(0, 10)}, ${data.Timestamp.substring(11, 19)}`;
+                `${convertToLocalTime(data.Timestamp)}`;
         })
         .catch(err => console.error('Error fetching latest location:', err));
 }
 
-// Fetch the latest location every 5 seconds
+function convertToLocalTime(utcDateString) {
+    const localDate = new Date(utcDateString); 
+    const options = {
+        day: '2-digit',
+        month: '2-digit',
+        year: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        hour12: false,
+        timeZone: 'America/Bogota'
+    };   
+    return localDate.toLocaleString('en-GB', options); // Format as needed
+}
+
+// Fetch the latest location every second
 setInterval(fetchLatestLocation, 1000);
 
 // Initial fetch
