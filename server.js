b040a346c7ffaefb519180188c80f3f9f2600a1b
kevin <kevintorregrosa2304@gmail.com>
2024-10-12 03:01:28 -0500

    final changes to testing environment (hopefully)

--- server.js
+++ server.js
@@ -80,19 +80,26 @@ app.get('/location-request', (req, res) => {
     });
 });
 
-// HTTPS server configuration
-https.createServer({
-    key: fs.readFileSync(`/etc/letsencrypt/live/${process.env.DOMAIN_NAME}/privkey.pem`),
-    cert: fs.readFileSync(`/etc/letsencrypt/live/${process.env.DOMAIN_NAME}/fullchain.pem`)
-}, app).listen(port, () => {
-    
-    console.log(`HTTPS Server running on https://localhost:${port}`);
-});
+if (port === 443) {
+    // HTTPS server configuration
+    https.createServer({
+        key: fs.readFileSync(`/etc/letsencrypt/live/${process.env.DOMAIN_NAME}/privkey.pem`),
+        cert: fs.readFileSync(`/etc/letsencrypt/live/${process.env.DOMAIN_NAME}/fullchain.pem`)
+    }, app).listen(port, () => {
+        
+        console.log(`HTTPS Server running on https://localhost:${port}`);
+    });
 
-// HTTP to HTTPS redirection (listen on port 80)
-http.createServer((req, res) => {
-    res.writeHead(301, { "Location": `https://${req.headers.host}${req.url}` });
-    res.end();
-}).listen(80, () => {
-    console.log(`HTTP server redirecting to HTTPS on port 80`);
-});
+    // HTTP to HTTPS redirection (listen on port 80)
+    http.createServer((req, res) => {
+        res.writeHead(301, { "Location": `https://${req.headers.host}${req.url}` });
+        res.end();
+    }).listen(80, () => {
+        console.log(`HTTP server redirecting to HTTPS on port 80`);
+    });
+} else {
+    // HTTP server configuration
+    app.listen(port, () => {
+        console.log(`HTTP Testing Server running on http://localhost:${port}`);
+    });
+}
