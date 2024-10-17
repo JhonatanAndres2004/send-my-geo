33b6e86aa3fbc108db20e31ce0f8d7ba82ec6e18
kevin <kevintorregrosa2304@gmail.com>
2024-10-17 02:42:16 -0500

    adjusted realtime to oop

--- public/scripts/Main.js
+++ public/scripts/Main.js
@@ -1,12 +1,18 @@
 import MapManager from './MapManager.js';
-import LocationService from './LocationService.js';
+import LocationServices from './LocationServices.js';
 import UIManager from './UIManager.js';
 
 const mapManager = new MapManager();
-const locationService = new LocationService();
+const locationService = new LocationServices();
 const uiManager = new UIManager();
+
+
+function liveLocation() {
+    uiManager.showTab('realtime');
+    locationService.startLiveLocation();
+
+}
 
 uiManager.loadName();
 mapManager.loadMap();
-uiManager.showTab('realtime');
-locationService.startLiveLocation();
+liveLocation();
